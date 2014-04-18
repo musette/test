62259336af2cff61796b9b3a93195447de8e0a59
musette <gn006sa+sourcetree@gmail.com>
2014-04-18 11:53:27 +0900

    first commit

--- src/com/example/hello2/MainActivity.java
+++ src/com/example/hello2/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.hello2;
 
+import android.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,7 +21,7 @@ public class MainActivity extends ActionBarActivity {
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
-
+	//test
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
